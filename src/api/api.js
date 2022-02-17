cee2c193c70dff9c3116862b535558364c4fe78b
Bruno <brunocmoraes97@gmail.com>
2022-02-16 22:12:28 -0300

    Implementa as pastas do msc e primeira rota da api

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  return res.status(200).json({ message: 'ok' });
+});
+
+module.exports = app;
---
+++ src/api/server.js
@@ -0,0 +1,5 @@
+const app = require('./api');
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`ouvindo na porta ${PORT}`))
