7a7e2b5d816358bce515237217b08ceea2baf73e
Bruno <brunocmoraes97@gmail.com>
2022-02-18 15:45:35 -0300

    Implementa configuraçoes do multer

--- src/routes/inventoryControl.js
+++ src/routes/inventoryControl.js
@@ -1,6 +1,7 @@
 const Router = require('express').Router();
 const { verifyQuantityToSales } = require('../controllers/stockController');
+const { validateJWT } = require('../middlewares/validateJWT');
 
-Router.get('/', verifyQuantityToSales);
+Router.get('/', validateJWT, verifyQuantityToSales);
 
 module.exports = Router;
--- src/routes/stock.js
+++ src/routes/stock.js
@@ -1,6 +1,21 @@
 const Router = require('express').Router();
-const { searchStock } = require('../controllers/stockController');
+const multer = require('multer');
 
-Router.get('/', searchStock);
+const { searchStock } = require('../controllers/stockController');
+const { validateJWT } = require('../middlewares/validateJWT');
+
+/* Configura o multer com o destino e o formato que será
+salvo o arquivo */
+const storage = multer.diskStorage({
+  destination: (req, _file, callback) => {
+    callback(null, 'src/uploads');
+  },
+  filename: (req, _file, callback) => {
+    const {id} = req.params;
+    callback(null, `${id}.jpeg`)
+  }
+});
+
+Router.get('/', validateJWT, searchStock);
 
 module.exports = Router;
