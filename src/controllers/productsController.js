38f745409d027ce5db37ca9800c1f8fe4ce7122c
Bruno <brunocmoraes97@gmail.com>
2022-02-21 22:04:41 -0300

    Corrige lint dos controllers

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -7,17 +7,17 @@ const { updateProductImage } = require("../services/products");
   Caso não exista. Cadastra o produto.
 */
 const createProducts = async (req, res) => {
-  const { name, image, price, ingredients } = req.body;
-  const verifyExistenceProduct = await findProductByName(name);
+	const { name, image, price, ingredients } = req.body;
+	const verifyExistenceProduct = await findProductByName(name);
 
-  if (verifyExistenceProduct) {
-    return res.status(409).json(
-      { message: `${name} already exists. Maybe the best is to edit it`}
-    )
-  }
+	if (verifyExistenceProduct) {
+		return res.status(409).json(
+			{ message: `${name} already exists. Maybe the best is to edit it`}
+		);
+	}
 
-  const newProduct = await generateProduct({name, image, price, ingredients});
-  return res.status(201).json(newProduct);
+	const newProduct = await generateProduct({name, image, price, ingredients});
+	return res.status(201).json(newProduct);
 };
 
 /* 
@@ -25,13 +25,13 @@ const createProducts = async (req, res) => {
   fazendo upload da imagem.
 */
 const editProductImage = async (req, res) => {
-  const { name } = req.params;
-  if (!name) return res.status(401).json({ message: 'Missing params valid!'})
-  const updateProduct = await updateProductImage(name);
-  return res.status(200).json(updateProduct);
+	const { name } = req.params;
+	if (!name) return res.status(401).json({ message: "Missing params valid!"});
+	const updateProduct = await updateProductImage(name);
+	return res.status(200).json(updateProduct);
 };
 
 module.exports = {
-  createProducts,
-  editProductImage,
+	createProducts,
+	editProductImage,
 };
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,26 +1,26 @@
-const md5 = require('md5');
+const md5 = require("md5");
 
-const { registerUser } = require('../models/userModel');
-const { findUser } = require('../services/users');
+const { registerUser } = require("../models/userModel");
+const { findUser } = require("../services/users");
 
 /* 
   Casa o usuário não exista, cria um usuário
   e encriptografa a senha.
 */
 const createUser = async (req, res) => {
-  const { name, password, email } = req.body;
-  const encryptPassword = md5(password);
-  const existenceUser = await findUser(email);
-  if (existenceUser) {
-    return res.status(409).json(
-      { message: 'Conflict. user already exists' }
-    )
-  }
+	const { name, password, email } = req.body;
+	const encryptPassword = md5(password);
+	const existenceUser = await findUser(email);
+	if (existenceUser) {
+		return res.status(409).json(
+			{ message: "Conflict. user already exists" }
+		);
+	}
 
-  const newUser = await registerUser({ name, email, password: encryptPassword });
-  return res.status(201).json(newUser);
+	const newUser = await registerUser({ name, email, password: encryptPassword });
+	return res.status(201).json(newUser);
 };
 
 module.exports = {
-  createUser,
+	createUser,
 };
