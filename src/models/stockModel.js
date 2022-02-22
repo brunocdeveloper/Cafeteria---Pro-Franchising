5d3cc801f78f331cafce952fa837db4d6282103b
Bruno <brunocmoraes97@gmail.com>
2022-02-21 22:03:21 -0300

    Instala Eslint e corrige o codigo para um padrao

--- src/models/stockModel.js
+++ src/models/stockModel.js
@@ -1,16 +1,16 @@
-const connection = require('./connection');
+const connection = require("./connection");
 
 const getProductStock = async () => {
-  const db = await connection();
-  const stockProducts = await db.collection('products').find({}).toArray();
-  return stockProducts;
+	const db = await connection();
+	const stockProducts = await db.collection("products").find({}).toArray();
+	return stockProducts;
 };
 
 const getIngredientStock = async () => {
-  const db = await connection();
-  const stockIngredients = await db.collection('ingredients').find({}).toArray();
-  return stockIngredients;
-}
+	const db = await connection();
+	const stockIngredients = await db.collection("ingredients").find({}).toArray();
+	return stockIngredients;
+};
 
 
 /* 
@@ -22,46 +22,43 @@ const getIngredientStock = async () => {
   em estoque seja maior que a quantidade para uso.
 */
 const getIngredientsWithJoin = async (ingredientName) => {
-  const db = await connection();
+	const db = await connection();
 
-  const stock = await db.collection('products').aggregate([
-    { $unwind: "$ingredients" },
-    {
-      $lookup: {
-        from: "ingredients",
-        let: { product_name: "$ingredients.name", product_quantity: "$ingredients.quantity" },
-        pipeline: [
-          { $match: { $expr: { $and:
-            [
-              { $eq: ["$name", "$$product_name"] },
-              { $gte: ["$quantity", "$$product_quantity"] },
-            ]
-          } } },
-          { $project: {
-            _id: 0,
-          }},
-        ],
-        as: "product_ingredients",
-      }
-    },
-    { $unwind: "$product_ingredients" },
-    { $match: { name: ingredientName }},
-    {
-      $project: {
-        _id: 0,
-        image: 0,
-        price: 0,
-      }
-    }
-  ]).toArray();
+	const stock = await db.collection("products").aggregate([
+		{ $unwind: "$ingredients" },
+		{
+			$lookup: {
+				from: "ingredients",
+				let: { product_name: "$ingredients.name", product_quantity: "$ingredients.quantity" },
+				pipeline: [
+					{ $match: { $expr: { $and:
+            [ { $eq: ["$name", "$$product_name"] },{ $gte: ["$quantity", "$$product_quantity"] }]
+					} } },
+					{ $project: {
+						_id: 0,
+					}},
+				],
+				as: "product_ingredients",
+			}
+		},
+		{ $unwind: "$product_ingredients" },
+		{ $match: { name: ingredientName }},
+		{
+			$project: {
+				_id: 0,
+				image: 0,
+				price: 0,
+			}
+		}
+	]).toArray();
 
-  return stock;
+	return stock;
 };
 
 
 
 module.exports = {
-  getProductStock,
-  getIngredientStock,
-  getIngredientsWithJoin,
+	getProductStock,
+	getIngredientStock,
+	getIngredientsWithJoin,
 };
